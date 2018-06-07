7678a981214d7bd7326a95fade135ccae651eb6b
Eden-Massalonga <38146156+Eden-Massalonga@users.noreply.github.com>
2018-06-07 03:24:43 -0700

    Remocao do Erro na Classe de Execucao
    O comando de execucao SpringApplication.run(); 
    chamava uma classe inexistente sgaGrupo1Application.class
    em vez de SgaGrupo1Application.class

--- src/main/java/mz/sgaspringapp/SgaGrupo1Application.java
+++ src/main/java/mz/sgaspringapp/SgaGrupo1Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SgaGrupo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(sgaGrupo1Application.class, args);
+		SpringApplication.run(SgaGrupo1Application.class, args);
 	}
 }
