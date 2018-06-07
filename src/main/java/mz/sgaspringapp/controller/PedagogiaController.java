3d40778a311080d05826288373caf33d9529207d
Mucumbe Blandino Sibone <mucmbeblandino@gmail.com>
2018-06-07 18:36:40 +0200

    melhoramento do Modulo pedagogico(Matricula)

--- src/main/java/mz/sgaspringapp/controller/PedagogiaController.java
+++ src/main/java/mz/sgaspringapp/controller/PedagogiaController.java
@@ -1,8 +1,14 @@
 package mz.sgaspringapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PedagogiaController {
-
+    
+@RequestMapping( value = "/Pedagogico")
+	public String cadastroClasse() {
+		return "cadastro/Pedagogico";
+	}
 }
